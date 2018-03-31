aca12e1685b28070b060c7ec8913fdd7dab085c7
Антон Степаненков <Lenseg1@gmail.com>
2018-03-31 20:10:42 +0900

    winnind number display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,17 @@ import { BetService } from './modules/bet/bet.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Endpass test-task';
+  winningNumber: number;
+  cacheNumber: number;
+  winningNumberSubject: BehaviorSubject<number>;
   hash: BehaviorSubject<string>;
   constructor(private betService: BetService) {
     this.hash = this.betService.hash;
+    this.winningNumberSubject = this.betService.winningNumber;
+    this.winningNumberSubject.subscribe((val) => {
+      this.winningNumber = this.cacheNumber;
+      this.cacheNumber = val;
+    });
   }
 }
